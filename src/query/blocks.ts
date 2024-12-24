800108b5482077bf6f3015439ba8f211abd45ee6
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-12-23 23:57:43 -0600

    fix: caching

--- src/query/blocks.ts
+++ src/query/blocks.ts
@@ -3,7 +3,9 @@ import { queryOptions } from "@tanstack/react-query";
 export const blockOptions = queryOptions({
   queryKey: ["blocks"],
   queryFn: async () => {
-    const response = await fetch("https://mempool.space/api/v1/blocks");
+    const response = await fetch("https://mempool.space/api/v1/blocks", {
+      next: { revalidate: 5 },
+    });
     return response.json();
   },
 });
